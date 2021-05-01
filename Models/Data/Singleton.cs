81537ff7c128877916269b8501c1661a25e5d750
martinbech <70785044+martinbechfurlan@users.noreply.github.com>
2021-05-01 03:43:04 -0600

    lista principal

---
+++ Models/Data/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_EDI.Models.Data
+{
+    public sealed class Singleton
+    {
+        private readonly static Singleton _instance = new Singleton();
+        public List<Personas> PersonasList;
+        private Singleton()
+        {
+            PersonasList = new List<Personas>();
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
